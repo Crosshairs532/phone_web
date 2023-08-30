3a46086a23247cd68d012df9c04a1c878cbbdab3
Md.Samsul Arefin <samsularefin532@gmail.com>
2023-08-30 13:31:40 +0600

    show all implemented

--- api.js
+++ api.js
@@ -1,28 +1,45 @@
 
 // searching
-const search = () => {
+const search = (bool) => {
     load_spinner(true);
     const search_text = document.getElementById('search_field').value;
-    load_value(search_text);
+    load_value(search_text, bool);
 
 }
 
 
 // loading value
-const load_value = async (value) => {
+const load_value = async (value, bool) => {
     const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${value}`);
     const data = await res.json();
     // console.log(data.data);
-    show_phones(data.data);
+    show_phones(data.data, bool);
 }
 load_value();
+// show all click
+const show = () => {
 
+    search(true);
+}
 
 // show-phones in card
-const show_phones = (phones) => {
+const show_phones = (phones, bool) => {
+
+    if (phones.length > 7 && !bool) {
+        const show_all = document.getElementById('show_all');
+        show_all.classList.remove('hidden');
+    }
+    else {
+        show_all.classList.add('hidden');
+    }
+    if (!bool) {
+        phones = phones.slice(0, 7);
+    }
+
 
     const phone_container = document.getElementById('Phone-container');
     phone_container.textContent = '';
+
     phones.forEach(phone => {
         const create_div_card = document.createElement('div');
         create_div_card.classList = "card w-96 bg-base-100 shadow-xl";
@@ -43,6 +60,7 @@ const show_phones = (phones) => {
 }
 // load_spinner
 const load_spinner = (isloading) => {
+    console.log(isloading);
     const loading = document.getElementById('loading');
     if (isloading) {
         loading.classList.remove('hidden');
